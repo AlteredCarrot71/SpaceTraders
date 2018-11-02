0ba785a053652022f830ada1af681c5b749fe880
AlteredCarrot71 <gabesz790409@gmail.com>
2018-11-02 15:43:29 +0100

    Maintainability update

--- SpaceTraders/MapScreen.xaml.cs
+++ SpaceTraders/MapScreen.xaml.cs
@@ -112,7 +112,7 @@ namespace SpaceTraders
             Game.Instance.CurrentPlanet = Game.Instance.Planets.Find(x => x.Name.Equals(ListPlanet.SelectedItem.ToString()));
             RandomEvent randomEvent = EventFactory.createRandomEvent(Game.Instance.Player);
             String even = randomEvent.Event();
-            if (!even.Equals("")) {
+            if (even.Length != 0) {
                 MessageDialog c = new MessageDialog(even, "Something has happened...");
                 await c.ShowAsync();
             }
@@ -137,39 +137,6 @@ namespace SpaceTraders
                 Travel.IsEnabled = false;
                 Travel.Content = "Too far ...";
             }
-        }
-    }
-
-    public class ColorList : List<Windows.UI.Color>
-    {
-        public ColorList()
-        {
-            this.Add(Windows.UI.Colors.Blue);
-            this.Add(Windows.UI.Colors.BlueViolet);
-            this.Add(Windows.UI.Colors.Brown);
-            this.Add(Windows.UI.Colors.CadetBlue);
-            this.Add(Windows.UI.Colors.Chartreuse);
-            this.Add(Windows.UI.Colors.Chocolate);
-            this.Add(Windows.UI.Colors.Coral);
-            this.Add(Windows.UI.Colors.CornflowerBlue);
-            this.Add(Windows.UI.Colors.Crimson);
-            this.Add(Windows.UI.Colors.Cyan);
-            this.Add(Windows.UI.Colors.LimeGreen);
-            this.Add(Windows.UI.Colors.Magenta);
-            this.Add(Windows.UI.Colors.Navy);
-            this.Add(Windows.UI.Colors.Olive);
-            this.Add(Windows.UI.Colors.OliveDrab);
-            this.Add(Windows.UI.Colors.Orange);
-            this.Add(Windows.UI.Colors.OrangeRed);
-            this.Add(Windows.UI.Colors.Orchid);
-            this.Add(Windows.UI.Colors.Red);
-            this.Add(Windows.UI.Colors.RosyBrown);
-            this.Add(Windows.UI.Colors.RoyalBlue);
-            this.Add(Windows.UI.Colors.Sienna);
-            this.Add(Windows.UI.Colors.Silver);
-            this.Add(Windows.UI.Colors.SkyBlue);
-            this.Add(Windows.UI.Colors.Yellow);
-            this.Add(Windows.UI.Colors.YellowGreen);
         }
     }
 }
---
+++ SpaceTraders/model/core/ColorList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace SpaceTraders
+{
+    public class ColorList : List<Windows.UI.Color>
+    {
+        public ColorList()
+        {
+            this.Add(Windows.UI.Colors.Blue);
+            this.Add(Windows.UI.Colors.BlueViolet);
+            this.Add(Windows.UI.Colors.Brown);
+            this.Add(Windows.UI.Colors.CadetBlue);
+            this.Add(Windows.UI.Colors.Chartreuse);
+            this.Add(Windows.UI.Colors.Chocolate);
+            this.Add(Windows.UI.Colors.Coral);
+            this.Add(Windows.UI.Colors.CornflowerBlue);
+            this.Add(Windows.UI.Colors.Crimson);
+            this.Add(Windows.UI.Colors.Cyan);
+            this.Add(Windows.UI.Colors.LimeGreen);
+            this.Add(Windows.UI.Colors.Magenta);
+            this.Add(Windows.UI.Colors.Navy);
+            this.Add(Windows.UI.Colors.Olive);
+            this.Add(Windows.UI.Colors.OliveDrab);
+            this.Add(Windows.UI.Colors.Orange);
+            this.Add(Windows.UI.Colors.OrangeRed);
+            this.Add(Windows.UI.Colors.Orchid);
+            this.Add(Windows.UI.Colors.Red);
+            this.Add(Windows.UI.Colors.RosyBrown);
+            this.Add(Windows.UI.Colors.RoyalBlue);
+            this.Add(Windows.UI.Colors.Sienna);
+            this.Add(Windows.UI.Colors.Silver);
+            this.Add(Windows.UI.Colors.SkyBlue);
+            this.Add(Windows.UI.Colors.Yellow);
+            this.Add(Windows.UI.Colors.YellowGreen);
+        }
+    }
+}
